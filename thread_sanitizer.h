c54e1add42bef0fbd724e5efbe27865effa9c29e
timurrrr <timurrrr@93a3164d-3a44-0410-8ae5-4baa038b0729>
2009-03-06 16:14:15 +0000

    cpplint'ed first 1000 lines of thread_sanitizer.cc

--- thread_sanitizer.h
+++ thread_sanitizer.h
@@ -48,6 +48,8 @@
 
 
 #define CHECK tl_assert
+#define CHECK_GT(X, Y) tl_assert(X > Y)
+#define CHECK_GE(X, Y) tl_assert(X >= Y)
 #define sprintf(arg1, arg2...) VG_(sprintf)((Char*)arg1, (HChar*)arg2)
 #define vsnprintf(a1, a2, a3, a4) VG_(vsnprintf)((Char*)a1, a2, a3, a4)
 #define getpid VG_(getpid)
